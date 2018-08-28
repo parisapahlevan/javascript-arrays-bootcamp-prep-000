e50aa74b44759fa61f9a2bdd6106755654b51bc2
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 12:14:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,39 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars = ["snickers", "hundred grand", "kitk","skittles"]
 function addElementToBeginningOfArray(array, element){
- array = ['foo', ...array]
-  return array
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift('foo')
+ array.unshift(element);
+ return array 
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
   return array
 }
-function addElementToEndOfArray()[
-  
-  ]
-  
-  function 
+
+function accessElementInArray(array, index){
+ return array[index] 
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array 
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+ array.pop()
+ return array  
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length-1)
+}
